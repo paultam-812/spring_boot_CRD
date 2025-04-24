fb92c5242b7dc8d3ad4efa0c488bf17a46d42142
paultam_812 <tony5888822@gmail.com>
2025-04-25 01:17:03 +0800

    DB schema setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book (title, author, published) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', true);
+INSERT INTO book (title, author, published) VALUES ('1984', 'George Orwell', true);
+INSERT INTO book (title, author, published) VALUES ('To Kill a Mockingbird', 'Harper Lee', true);
+INSERT INTO book (title, author, published) VALUES ('The Catcher in the Rye', 'J.D. Salinger', false);
+INSERT INTO book (title, author, published) VALUES ('Moby Dick', 'Herman Melville', true);
